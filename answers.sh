64eb235160b863cd348723688a4e6eb8259e5bce
Patralika Ghosh <ghoshpatra2002@gmail.com>
2024-10-08 22:58:16 -0500

    Creating Table

--- answers.sh
+++ answers.sh
@@ -16,5 +16,6 @@ CREATE TABLE graduates(
 );
 
 INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
-UPDATE graduates SET Graduation='10-08-2024' WHERE Name='Layal';
+UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
 DELETE FROM graduates WHERE Name='Layal';
+
