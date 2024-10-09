d0357d87b5773f79dcd9c96aa5539c666d85ebdf
Patralika Ghosh <ghoshpatra2002@gmail.com>
2024-10-08 23:06:45 -0500

    Joins

--- answers.sh
+++ answers.sh
@@ -4,7 +4,7 @@ SELECT Name FROM Students WHERE Gender='F' AND Age=30;
 SELECT Points FROM Students WHERE Name='Alex';
 INSERT INTO Students VALUES(10, 'Patralika', '22', 'F', 500);
 UPDATE Students SET Points = 500 WHERE Name='Basma';
-UPDATE Students SET Points = 100 WHERE Name='Alex';
+UPDATE Students SET Points = 50 WHERE Name='Alex';
 
 CREATE TABLE graduates(
     ID INTEGER  PRIMARY KEY AUTOINCREMENT,
@@ -18,4 +18,8 @@ CREATE TABLE graduates(
 INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
 UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
 DELETE FROM graduates WHERE Name='Layal';
+
+SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
+SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
+SELECT companies.name FROM companies Inner join employees ON employees.Company=companies.name and employees.Role='Graphic Designer';
 
